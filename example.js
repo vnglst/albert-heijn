5838594c3d6869ee0058cbb931fe339320f9e0e9
Koen van Gilst <koen.vangilst@gmail.com>
2017-05-26 20:49:05 +0200

    adds functions for price and product info
    - refactor using async await

--- example.js
+++ example.js
@@ -1,8 +1,7 @@
 const ah = require('./index');
 
-const url = 'https://www.ah.nl/producten/product/wi33693/ah-halfvolle-melk';
-
-ah.getNutritionFacts(url).then(console.log);
+const urlMelk = 'https://www.ah.nl/producten/product/wi33693/ah-halfvolle-melk';
+ah.getNutritionFacts(urlMelk).then(console.log);
 
 /*
 { Energie: '200 kJ (48 kcal)',
@@ -19,3 +18,31 @@ ah.getNutritionFacts(url).then(console.log);
   'Vitamine B12': '0,47 µg',
   Calcium: '126 mg' }
 */
+
+const urlAsperges = 'https://www.ah.nl/producten/product/wi99045/ah-asperges-wit';
+ah.getPriceInfo(urlAsperges, 'wi99045').then(console.log);
+
+/*
+{ price: 2.99,
+  unit: '500 g',
+  availability: { orderable: true, label: 'Leverbaar' } }
+*/
+
+ah.getProductInfo(urlAsperges, 'wi99045').then(console.log);
+
+/*
+{ id: 'wi99045',
+  description: 'AH As­per­ges wit',
+  unitSize: '500 g',
+  brandName: 'AH',
+  categoryName: 'Aardappel, groente, fruit/Witte asperges',
+  availability: { orderable: true, label: 'Leverbaar' },
+  priceLabel: { now: 2.99, was: 3.99 },
+  discount:
+   { type: { name: 'BONUS' },
+     label: '25% korting',
+     period: 'vanaf maandag' },
+  propertyIcons: [ 'gezondere_keuze' ],
+  ... etc.
+*/
+
--- index.js
+++ index.js
@@ -1,62 +1,90 @@
-const fetch = require('isomorphic-fetch');
+const fetch = require("isomorphic-fetch")
+const urlTools = require("url")
+const traverse = require("traverse")
+const jsdom = require("jsdom")
+const _ = require("lodash")
 
-const urlTools = require('url');
-
-const traverse = require('traverse');
-
-const jsdom = require('jsdom');
-
-const {
-  zipObject,
-} = require('lodash');
-
-const parseColumn = (column) => Array.from(column).map(tr => tr.textContent);
-
-const getJSONUrl = (url) => {
-  const { pathname } = urlTools.parse(url);
-
-  return `https://www.ah.nl/service/rest/delegate?url=${encodeURIComponent(pathname)}`;
+/* 
+  Deep search an object for a certain key
+   -> returns array of objects containing key
+*/
+function findKey(obj, key) {
+  if (_.has(obj, key)) return [obj]
+  return _.flatten(
+    _.map(obj, function(v) {
+      return typeof v == "object" ? findKey(v, key) : []
+    }),
+    true
+  )
 }
 
-const getNutritionFacts = (url) =>
-  fetch(getJSONUrl(url))
-  .then(response => {
-    if (response.status >= 400) {
-      throw new Error('Bad response from server');
-    }
-    return response.json();
+const getJSONUrl = url => {
+  const { pathname } = urlTools.parse(url)
+  return `https://www.ah.nl/service/rest/delegate?url=${encodeURIComponent(pathname)}`
+}
+
+const getJSON = async url => {
+  const response = await fetch(getJSONUrl(url))
+  if (response.status >= 400) {
+    throw new Error("Bad response from server")
+  }
+  return response.json()
+}
+
+const getProductInfo = async (url, productId) => {
+  const json = await getJSON(url)
+  const allProducts = findKey(json, "id")
+  const productInfo = allProducts.find(product => product.id === productId)
+  return productInfo
+}
+
+const getPriceInfo = async (url, productId) => {
+  const productInfo = await getProductInfo(url, productId)
+  return {
+    price: productInfo.priceLabel.now,
+    unit: productInfo.unitSize,
+    availability: productInfo.availability
+  }
+}
+
+const parseColumn = column => Array.from(column).map(tr => tr.textContent)
+
+const parseHTMLTable = html =>
+  new Promise(resolve => {
+    jsdom.env(html, [], (err, window) => {
+      const { document } = window
+
+      const labels = parseColumn(
+        document.querySelectorAll("tr > td:nth-of-type(1)")
+      )
+
+      const values = parseColumn(
+        document.querySelectorAll("tr > td:nth-of-type(2)")
+      )
+
+      resolve(_.zipObject(labels, values))
+    })
   })
-  .then(json => {
-    let compositionString = '';
 
-    traverse(json._embedded.lanes).forEach(function() {
-      if (typeof this.node === 'string' && this.node.includes('Eiwitten')) {
-        compositionString = this.node;
-      }
-    });
+const getNutritionFacts = async url => {
+  const json = await getJSON(url)
+  // console.log(json)
+  let compositionString = ""
 
-    const html =
-      compositionString
-        .replace(/\[/g, '<')
-        .replace(/\]/g, '>');
+  traverse(json._embedded.lanes).forEach(function() {
+    if (typeof this.node === "string" && this.node.includes("Eiwitten")) {
+      compositionString = this.node
+    }
+  })
 
-    return new Promise(resolve => {
-      jsdom.env(html, [], (err, window) => {
-        const { document } = window;
+  const html = compositionString.replace(/\[/g, "<").replace(/\]/g, ">")
+  const nutritionTable = await parseHTMLTable(html)
 
-        const labels =
-          parseColumn(document.querySelectorAll('tr > td:nth-of-type(1)'));
-
-        const values =
-          parseColumn(document.querySelectorAll('tr > td:nth-of-type(2)'));
-
-        resolve(
-          zipObject(labels, values)
-        );
-      });
-    });
-  });
+  return nutritionTable
+}
 
 module.exports = {
   getNutritionFacts,
-};
+  getPriceInfo,
+  getProductInfo
+}

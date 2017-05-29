7f6cfb39ae826eca5fedf5cd036b932ad9e5b5ed
Koen van Gilst <koen.vangilst@gmail.com>
2017-05-29 12:13:31 +0200

    removes getNutritionFacts since it depends on jsdom

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const fetch = require("isomorphic-fetch")
 const urlTools = require("url")
-const traverse = require("traverse")
-const { zipObject, map, has, flatten } = require("lodash")
+const { map, has, flatten } = require("lodash")
 
-let jsdom, JSDOM
-if (typeof window === "undefined") {
-  jsdom = require("jsdom")
-  JSDOM = jsdom.JSDOM
-}
 /* 
   Deep search an object for a certain key
    -> returns array of objects containing key
@@ -51,42 +45,7 @@ const getPriceInfo = async (url, productId) => {
   }
 }
 
-const parseColumn = column => Array.from(column).map(tr => tr.textContent)
-
-const parseHTMLTable = html => {
-  if (typeof window === "undefined") {
-    const dom = new JSDOM(html)
-    var { document } = dom.window
-  }
-
-  const labels = parseColumn(
-    document.querySelectorAll("tr > td:nth-of-type(1)")
-  )
-
-  const values = parseColumn(
-    document.querySelectorAll("tr > td:nth-of-type(2)")
-  )
-  return zipObject(labels, values)
-}
-
-const getNutritionFacts = async url => {
-  const json = await getJSON(url)
-  let compositionString = ""
-
-  traverse(json._embedded.lanes).forEach(function() {
-    if (typeof this.node === "string" && this.node.includes("Eiwitten")) {
-      compositionString = this.node
-    }
-  })
-
-  const html = compositionString.replace(/\[/g, "<").replace(/\]/g, ">")
-  const nutritionTable = await parseHTMLTable(html)
-
-  return nutritionTable
-}
-
 module.exports = {
-  getNutritionFacts,
   getPriceInfo,
   getProductInfo
 }
